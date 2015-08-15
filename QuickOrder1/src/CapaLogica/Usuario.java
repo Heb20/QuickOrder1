e0514cb81e711c2f8103e37fe3b3b0e1a4697914
natalie <natalie@localhost>
2015-08-15 18:24:42 -0300

    v 0.0.08 Clase Usuario (simple, getters y setters)

--- QuickOrder1/src/CapaLogica/Usuario.java
+++ QuickOrder1/src/CapaLogica/Usuario.java
@@ -1,11 +1,75 @@
 package CapaLogica;
 
-
 public class Usuario {
 
    
-    public static void main(String[] args) {
+   private String nickname;    
+   private String nombre;
+   private String email;
+   private String direccion;
+   
+   //Constructor
+   public Usuario (){
+      
+      this.nickname="";
+      this.nombre="";
+      this.email="";
+      this.direccion="";
+      
+   
+   };
+   
+   public Usuario (String nickname, String nombre, String email, String direccion){
+      
+      this.nickname= nickname;
+      this.nombre=nombre;
+      this.email=email;
+      this.direccion=direccion;
+      
+   };
+
+   //Setters
+   
+   public void setNickname(String nickname){
        
-    }
-    
+       this.nickname=nickname;
+   
+   }
+   
+   public void setNombre(String nombre){
+       
+       this.nombre=nombre;
+   
+   }
+   
+   public void setEmail(String email){
+       
+       this.email=email;
+   
+   }
+   
+   public void setDireccion(String direccion){
+       
+       this.direccion=direccion;
+   
+   }
+   
+   //Getters
+   
+   public String getNickname(){
+       return nickname;
+   }
+   
+   public String getNombre(){
+       return nombre;
+   }
+   
+   public String getEmail(){
+       return email;
+   }
+   
+   
+   public String getDireccion(){
+       return direccion;
+   }
 }
