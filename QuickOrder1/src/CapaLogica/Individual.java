e37027e3c35448548d2188247a2fec424ca4abd3
alejandra <alejandra@localhost>
2015-08-16 05:20:10 -0300

    Prueba Commit

---
+++ QuickOrder1/src/CapaLogica/Individual.java
@@ -0,0 +1,12 @@
+
+package CapaLogica;
+
+//Alejandra
+public class Individual extends Productos{
+    
+    public Individual(String nombre,String descripcion,float precio,String imagen){
+        super(nombre, descripcion,precio,imagen); 
+        
+    }
+    
+}
